6efa2380fac6474fee78d23ca451e921c36690b4
rory <rory_bell_88@hotmail.com>
2020-10-29 08:37:35 +1000

    bender can now be successfully placed and the loop works as expected for manu items

--- robot.rb
+++ robot.rb
@@ -1,10 +1,23 @@
 class BenderBendingRodriguez
 
-  attr_accessor :x_axis, :y_axis, :facing
+  attr_accessor :x_axis, :y_axis, :facing, :place_bender, :placed
   
-  def initialize(obj)
-    @x_axis = obj[:x]
-    @y_axis = obj[:y]
-    @facing = obj[:bearing]
+  def initialize()
+    @x_axis = nil
+    @y_axis = nil
+    @facing = nil
+    @placed = false
   end
+
+  def place_bender(placement_object)
+    if !@placed 
+      @placed = true
+    end
+    @x_axis = placement_object[:x]
+    @y_axis = placement_object[:y]
+    @facing = placement_object[:bearing]
+    puts "Beep Boop, Bender is on the board."
+  end
+
+
 end
