5237bea436fb82c7092fd91b132abfc791b5ae81
rory <rory_bell_88@hotmail.com>
2020-10-28 20:43:15 +1000

    as per challenge specs 0 should be the initial index

--- command_validator.rb
+++ command_validator.rb
@@ -6,7 +6,7 @@ class CommandValidator
 	  return true if %W(MOVE LEFT RIGHT REPORT).include? striped_command
 	  if striped_command.split[0] == "PLACE" 
 		puts striped_command.match(/^PLACE [1-5],[1-5],(NORTH|EAST|SOUTH|WEST)$/)
-        return true if striped_command.match(/^PLACE [1-5],[1-5],(NORTH|EAST|SOUTH|WEST)$/)
+        return true if striped_command.match(/^PLACE [0-4],[1-4],(NORTH|EAST|SOUTH|WEST)$/)
 	  end
 	  raise InvalidCommandError
 	end
